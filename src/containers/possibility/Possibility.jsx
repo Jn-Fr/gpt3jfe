cf028aadeca60e506b5fe77ac8a6deb75474753e
Juan Espin <juan_espin1995@hotmail.es>
2022-03-31 23:34:21 -0500

    first commit

---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './possibility.css';
+import possibilityImage from '../../assets/possibility.png';
+
+const Possibility = () => {
+  return (
+    <div className='gpt3__possibility section__padding' id='possibility'>
+      <div className='gpt3__possibility-image'>
+        <img src={possibilityImage} alt="possibility" />
+      </div>
+      <div className='gpt3__possibility-content'>
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className='gradient__text'>The possibilites are beyond your imagination</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt vel maxime, voluptatibus reiciendis sint officia quidem vero voluptatem libero obcaecati quis ullam beatae eligendi quam nostrum doloremque explicabo cumque sapiente.</p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
+    </div>
+  )
+};
+
+export default Possibility;
